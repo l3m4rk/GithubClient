44df6bcae0a055d1f9dfa7663ff4f8a09f3ce24f
L3m4rk <b4r4n0v@gmail.com>
2017-07-19 23:04:19 +0300

    Moves main activity to user package and renames it to UserActivity

---
+++ app/src/main/java/example/l3m4rk/edu/githubclient/presentation/user/views/UserActivity.kt
@@ -0,0 +1,13 @@
+package example.l3m4rk.edu.githubclient.presentation.user.views
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import example.l3m4rk.edu.githubclient.R
+
+class UserActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user)
+    }
+}
