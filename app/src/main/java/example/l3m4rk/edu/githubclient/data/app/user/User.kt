a645f2636b80da2c5818c0df15413778eae7cfd8
L3m4rk <b4r4n0v@gmail.com>
2017-07-22 21:39:39 +0300

    Adds User app model

---
+++ app/src/main/java/example/l3m4rk/edu/githubclient/data/app/user/User.kt
@@ -0,0 +1,8 @@
+package example.l3m4rk.edu.githubclient.data.app.user
+
+data class User(
+        val login: String,
+        val password: String,
+        val avatarUrl: String,
+        val authToken: String
+)
