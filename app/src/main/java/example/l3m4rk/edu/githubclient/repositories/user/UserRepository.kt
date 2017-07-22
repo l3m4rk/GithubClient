3d1a12fff870bf21cf78a65cf6ed47d89d9b121e
L3m4rk <b4r4n0v@gmail.com>
2017-07-23 00:48:08 +0300

    refactoring

--- app/src/main/java/example/l3m4rk/edu/githubclient/repositories/user/UserRepository.kt
+++ app/src/main/java/example/l3m4rk/edu/githubclient/repositories/user/UserRepository.kt
@@ -21,19 +21,18 @@ class UserRepository(context: Context) : IUserRepository {
         preferences.edit().apply {
             putString(KEY_LOGIN, user.login)
             putString(KEY_PASSWORD, user.password)
-            putString(KEY_TOKEN, user.authToken)
+            putString(KEY_AVATAR_URL, user.avatarUrl)
+//            putString(KEY_TOKEN, user.authToken)
         }.apply()
         return Result.Success()
     }
 
     override fun loadUser(): Single<User> {
-        return Single.fromCallable {
-            val login = preferences.getString(KEY_LOGIN, "")
-            val password = preferences.getString(KEY_PASSWORD, "")
-            val avatarUrl = preferences.getString(KEY_AVATAR_URL, "")
-            val token = preferences.getString(KEY_TOKEN, "")
-            User(login, password, avatarUrl, token)
-        }
+        val login = preferences.getString(KEY_LOGIN, "")
+        val password = preferences.getString(KEY_PASSWORD, "")
+        val avatarUrl = preferences.getString(KEY_AVATAR_URL, "")
+        val token = preferences.getString(KEY_TOKEN, "")
+        return Single.just(User(login, password, avatarUrl, token))
     }
 
 }
