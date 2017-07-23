e44b7e0958c3b4efad0fb0c39f00a102dc71e042
L3m4rk <b4r4n0v@gmail.com>
2017-07-23 21:10:41 +0300

    Provide support fragment injection to UserActivity

--- app/src/main/java/example/l3m4rk/edu/githubclient/presentation/user/views/UserActivity.kt
+++ app/src/main/java/example/l3m4rk/edu/githubclient/presentation/user/views/UserActivity.kt
@@ -2,19 +2,25 @@ package example.l3m4rk.edu.githubclient.presentation.user.views
 
 import android.content.Intent
 import android.os.Bundle
+import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
 import dagger.android.AndroidInjection
+import dagger.android.AndroidInjector
+import dagger.android.DispatchingAndroidInjector
+import dagger.android.support.HasSupportFragmentInjector
 import example.l3m4rk.edu.githubclient.R
 import example.l3m4rk.edu.githubclient.data.app.user.User
 import example.l3m4rk.edu.githubclient.presentation.login.views.LoginActivity
 import example.l3m4rk.edu.githubclient.presentation.repos.models.RepoItem
 import example.l3m4rk.edu.githubclient.presentation.repos.views.ReposFragment
-import example.l3m4rk.edu.githubclient.presentation.repos.views.dummy.DummyContent
 import example.l3m4rk.edu.githubclient.presentation.user.presenter.IUserPresenter
 import kotlinx.android.synthetic.main.activity_user.*
 import javax.inject.Inject
 
-class UserActivity : AppCompatActivity(), UserView, ReposFragment.OnReposInteractionListener {
+class UserActivity : AppCompatActivity(), UserView, ReposFragment.OnReposInteractionListener, HasSupportFragmentInjector {
+
+    @Inject lateinit var fragmentInjector: DispatchingAndroidInjector<Fragment>
+
     @Inject lateinit var userPresenter: IUserPresenter
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,6 +28,10 @@ class UserActivity : AppCompatActivity(), UserView, ReposFragment.OnReposInterac
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user)
         setSupportActionBar(toolbar)
+    }
+
+    override fun supportFragmentInjector(): AndroidInjector<Fragment> {
+        return fragmentInjector
     }
 
     override fun onStart() {
