79de09705ac0d392576000acb67eb75f2ddf0484
L3m4rk <b4r4n0v@gmail.com>
2017-07-23 21:13:56 +0300

    Inject ReposPresenter

--- app/src/main/java/example/l3m4rk/edu/githubclient/presentation/repos/views/ReposFragment.kt
+++ app/src/main/java/example/l3m4rk/edu/githubclient/presentation/repos/views/ReposFragment.kt
@@ -9,13 +9,19 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import dagger.android.support.AndroidSupportInjection
 import example.l3m4rk.edu.githubclient.R
 import example.l3m4rk.edu.githubclient.presentation.repos.models.RepoItem
+import example.l3m4rk.edu.githubclient.presentation.repos.presenter.IReposPresenter
+import javax.inject.Inject
 
 class ReposFragment : Fragment(), ReposView {
     private var mListener: OnReposInteractionListener? = null
 
+    @Inject lateinit var reposPresenter: IReposPresenter
+
     private lateinit var reposAdapter: ReposAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -38,12 +44,19 @@ class ReposFragment : Fragment(), ReposView {
     }
 
     override fun onAttach(context: Context?) {
+        AndroidSupportInjection.inject(this)
         super.onAttach(context)
         if (context is OnReposInteractionListener) {
             mListener = context
         } else {
             throw RuntimeException(context!!.toString() + " must implement OnListFragmentInteractionListener")
         }
+        reposPresenter.bindView(this)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        reposPresenter.loadRepos()
     }
 
     override fun showProgress() {
@@ -54,6 +67,10 @@ class ReposFragment : Fragment(), ReposView {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
+    override fun showError() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
     override fun showRepos(repos: List<RepoItem>) {
         reposAdapter.update(repos)
     }
@@ -61,10 +78,12 @@ class ReposFragment : Fragment(), ReposView {
     override fun onDetach() {
         super.onDetach()
         mListener = null
+        reposPresenter.unbindView()
     }
 
     interface OnReposInteractionListener {
         fun onRepoClicked(item: RepoItem)
+
     }
 
     companion object {
