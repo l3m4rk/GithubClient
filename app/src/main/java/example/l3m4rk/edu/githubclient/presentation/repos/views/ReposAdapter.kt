34abf14aacecc24baedf509566315769a52d9207
L3m4rk <b4r4n0v@gmail.com>
2017-07-23 03:21:11 +0300

    Adds layout file for repo

--- app/src/main/java/example/l3m4rk/edu/githubclient/presentation/repos/views/ReposAdapter.kt
+++ app/src/main/java/example/l3m4rk/edu/githubclient/presentation/repos/views/ReposAdapter.kt
@@ -12,6 +12,8 @@ import example.l3m4rk.edu.githubclient.presentation.repos.views.dummy.DummyConte
 
 class ReposAdapter(private val mValues: List<DummyItem>, private val mListener: OnReposInteractionListener?) : RecyclerView.Adapter<ReposAdapter.RepoViewHolder>() {
 
+
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RepoViewHolder {
         val view = LayoutInflater.from(parent.context)
                 .inflate(R.layout.item_repo, parent, false)
@@ -23,6 +25,7 @@ class ReposAdapter(private val mValues: List<DummyItem>, private val mListener: 
         repoHolder.mNameView.text = mValues[position].id
         repoHolder.mDescriptionView.text = mValues[position].content
 
+
         repoHolder.mView.setOnClickListener {
             mListener?.onRepoClicked(repoHolder.mItem as DummyItem)
         }
@@ -33,13 +36,19 @@ class ReposAdapter(private val mValues: List<DummyItem>, private val mListener: 
     }
 
     inner class RepoViewHolder(val mView: View) : RecyclerView.ViewHolder(mView) {
+        val mAuthorView: TextView
         val mNameView: TextView
         val mDescriptionView: TextView
+        val mForksView: TextView
+        val mWatchesView: TextView
         var mItem: DummyItem? = null
 
         init {
+            mAuthorView = mView.findViewById(R.id.author) as TextView
             mNameView = mView.findViewById(R.id.name) as TextView
             mDescriptionView = mView.findViewById(R.id.description) as TextView
+            mForksView = mView.findViewById(R.id.forks) as TextView
+            mWatchesView = mView.findViewById(R.id.watches) as TextView
         }
 
         override fun toString(): String {
